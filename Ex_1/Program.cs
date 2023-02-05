f177769fe3307fb93cf9f96fb1a736a2e9c2ab16
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-02-05 21:16:31 +0300

    Задача №1. Написать программу, упорядочивания по убыванию элементов каждой строки двумерного массива. Задача решена.

---
+++ Ex_1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача №1. Написать программу, упорядочивания по убыванию элементов каждой строки двумерного массива.
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " | ");
+        }        
+        Console.WriteLine("");
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк двумерного массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов двумерного массива: ");
+int column = int.Parse(Console.ReadLine()!);
+
+int[,] numbers = new int[rows, column];
+
+FillArray(numbers);
+Console.WriteLine();
+Console.WriteLine("Исходный массив: ");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < numbers.GetLength(1) - 1; k++)
+        {
+            if (numbers[i, k] < numbers[i, k + 1])
+            {                
+                int temp = numbers[i, k];
+                numbers[i, k] = numbers[i, k + 1];
+                numbers[i, k + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями: ");
+PrintArray(numbers);
